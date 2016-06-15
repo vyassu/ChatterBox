e876e0b8760ffa02bef6c1d7be436866f5ab1f8f
Vyas Kovakkat <vyas.kovakkat@ufl.edu>
2016-06-14 23:48:44 -0400

    Added logic for Plain Authentication

--- XMPPSSLConn_Openfire.py
+++ XMPPSSLConn_Openfire.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 0):
     sys.setdefaultencoding('utf8')
 
 class XMPPConnMgr(sleekxmpp.ClientXMPP):
-    def __init__(self, jid, password):
+    def __init__(self, jid, password,encryption=True):
         sleekxmpp.ClientXMPP.__init__(self, jid, password,sasl_mech="PLAIN")
         # Plugins that needs to be registered
         self.register_plugin('xep_0030')  # Service Discovery
@@ -20,15 +20,19 @@ class XMPPConnMgr(sleekxmpp.ClientXMPP):
         #   Ca certs points to the system trustore located at /etc/ssl/certs
         #   certfile points to the certificate file of the client
         #   keyfile points to the key file of the client
-        self.ca_certs = "/etc/ssl/certs/ca-certificates.crt"
-        self.certfile = "/home/vyassu/cert/Openfire/node0@localhost.crt"
-        self.keyfile = "/home/vyassu/cert/Openfire/node0@localhost.key"
+
+        if encryption:
+            self.ca_certs = "/etc/ssl/certs/ca-certificates.crt"
+            self.certfile = "/home/vyassu/cert/Openfire/node0@localhost.crt"
+            self.keyfile = "/home/vyassu/cert/Openfire/node0@localhost.key"
+        else:
+            self['feature_mechanisms'].unencrypted_plain = True
         self.roaster = {}                                                   # Variable to store all friends/peer
         self.add_event_handler("session_start", self.start)                 # Registering event handler
 
     # Method to create connection to XMPP server
-    def conn(self):
-        if conn.connect(("localhost", "6222"), use_tls=True):
+    def conn(self,useTLS=True):
+        if conn.connect(("localhost", "6222"), use_tls=useTLS):
             conn.process(block=False)
             print("Connection established....")
             print ("JID after connection:: ", conn.jid)
@@ -42,7 +46,7 @@ class XMPPConnMgr(sleekxmpp.ClientXMPP):
         if msg['type'] in ('chat', 'normal'):
            if msg['subject']!="":
              msg.reply("Got it !!\n%(body)s" % msg).send()
-        return msg['body']
+        print str(msg['body'])
 
     # Method to update the status of all the peers online
     def online_friends(self,presence):
@@ -65,15 +69,24 @@ class XMPPConnMgr(sleekxmpp.ClientXMPP):
 
     # Method to send messages to Friends
     def SendMessage(self,msg,recepient):
-        if self.roaster[recepient]!="Online":
-            print "Friend offline.."
+        try:
+            if len(self.roaster)==0:
+                print "No friends in the Roster!!"
+                return
+            if self.roaster[recepient]!="Online":
+                print "Friend offline.."
 
-        message = self.Message()
-        message['to'] = recepient
-        message['type'] = 'chat'
-        message['body'] = msg
-        message['subject']= msg
-        message.send()
+            message = self.Message()
+            message['to'] = recepient
+            message['type'] = 'chat'
+            message['body'] = msg
+            message['subject']= msg
+            message.send()
+
+        except KeyError:
+            print "Invalid Destination/ FriendName"
+        except:
+            print "Unhandled exception"
 
 
 
@@ -83,15 +96,21 @@ if __name__ == '__main__':
     optp.add_option('-p', '--password', help='your password', dest='password')
     opts, args = optp.parse_args()
 
-    logging.basicConfig(level=logging.INFO,format='%(levelname)-8s %(message)s')
+    logging.basicConfig(level=logging.DEBUG,format='%(levelname)-8s %(message)s')
 
     if opts.username is None or opts.password is None:
         raise RuntimeError("No username/password provided!!!")
 
-    conn = XMPPConnMgr(opts.username, opts.password)        ## Connection manager
+    print "Do you want encryption ON/OFF:"
+    useTLS=True
+    encryption= raw_input()
+    if encryption=="OFF":
+        useTLS=False
+
+    conn = XMPPConnMgr(opts.username, opts.password,useTLS)        ## Connection manager
 
     # Start sending messages only after successful connection with XMPP server
-    if conn.conn():
+    if conn.conn(useTLS):
         print "Ready to send message!!!"
         print "Enter friend/target system name/JID"
         recepient = raw_input()
@@ -102,6 +121,7 @@ if __name__ == '__main__':
             conn.SendMessage(message,recepient)
             print "Do you to continue(Y/N):"
             exit = raw_input()
+    conn.disconnect(wait=True)
     #conn.keyfile="/home/vyassu/cert/Openfire/vyassu@nm.vyassu-qv15971.ufl-eel6892-sp16-pg0.wisc.cloudlab.us_in.key"
     #conn.certfile="/home/vyassu/cert/Openfire/vyassu@nm.vyassu-qv15971.ufl-eel6892-sp16-pg0.wisc.cloudlab.us.crt"
 
